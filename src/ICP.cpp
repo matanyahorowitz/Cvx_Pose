2ff6562d3fde114b2e2b8a65f8dc3b7b78e55bc8
Matanya Horowitz <matanya.horowitz@gmail.com>
2014-07-17 15:42:20 -0600

    Continue adding details
    Continued to flesh out program structure. Deleted KDTree, we are going
    to use PCL’s implementation, and add PCL as a dependency.

--- src/ICP.cpp
+++ src/ICP.cpp
@@ -10,30 +10,61 @@
 
 ICP::ICP()
 {
-
+    i_R = Matrix4f::Identity();
+    i_T << 0,0,0;
 }
 
-void ICP::setModel()
+ICP::~ICP()
 {
-
+    kd_free( this->tree );
 }
 
-void ICP::setSolver()
+void ICP::setSolver(SolverSettings s)
 {
-    
+    this->settings = s;
 }
 
-void ICP::setObservation()
+void ICP::setModel( DMat & m )
 {
-    
+    this->num_pts = m.cols();
+    this->model = m;
+    this->initKDTree();
+}
+
+void ICP::setObservation( DMat & o )
+{
+    this->observation = o;
+}
+
+void ICP::initKDTree()
+{
+    this->tree = kd_create(3);
+    for (int i=0; i<this->num_pts; i++) {
+        kd_insert3f( this->tree, model(1,i), model(2,i), model(3,i) );
+    }
 }
 
 void ICP::align()
 {
+    c_R = i_R;
+    c_T = i_T;
     
+    //First do a coarse alignment.
+    
+    //Second, do a fine alignment.
 }
 
+
 void ICP::singleIteration()
+{
+    //Solve correspondence
+    
+    //First, transform according to current pose estimate
+    DMat data = c_R * observation + c_T * MatrixXd::Constant(1,observation.cols());
+    Vector1d match = solveCorrespondence(data);
+}
+
+Vector1d ICP::solveCorrespondence( DMat & data )
 {
     
 }
--- src/ICP.h
+++ src/ICP.h
@@ -10,6 +10,10 @@
 #define ____ICP__
 
 #include <iostream>
+#include <Eigen/Dense>
+
+typedef Matrix<float,3,dynamic> DMat;
+
 struct SolverSettings {
     int metric; //P2P CVX, P2P Analytic, P2Plane CVX
     bool outlierRejection;
@@ -18,6 +22,17 @@ struct SolverSettings {
 };
 
 class ICP {
-    
+public:
+    ICP();
+    ~ICP();
+    void setModel();
+private:
+    SolverSettings settings;
+    PoseEstimate pose;
+    Matrix3f c_R, i_R; //current, initial rotation
+    Vector3f c_T, i_T; //translation
+    kdtree tree;
+    DMat model;
+    DMat observation;
 };
 #endif /* defined(____ICP__) */
